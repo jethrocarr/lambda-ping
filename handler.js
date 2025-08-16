2ffe6486e4b61aa13f3270da1f5aa4c60aa701b1
Jethro Carr <jethro.carr@jethrocarr.com>
2025-08-16 23:19:23 +1200

    Updated the codebase and dependencies of the project [AI]
    Prompt: Update the application code to follow latest JS standards, as well as latest AWS SDK

--- handler.js
+++ handler.js
@@ -1,126 +1,81 @@
 'use strict';
 
-module.exports.http = (event, context, callback) => {
-  var output  = {};
+const axios = require('axios');
+const AWS = require('aws-sdk');
 
-  // We use the https://www.npmjs.com/package/request package for our HTTP
-  // calls since it makes things so easy.
-  var request  = require('request');
+const cloudwatch = new AWS.CloudWatch();
 
-  // The AWS SDK is shipped as part of the Lambda environment. We need it in
-  // order to post metrics to CloudWatch.
-  var AWS = require('aws-sdk');
+module.exports.http = async (event, context) => {
+  let output = {};
 
-  var cloudwatch = new AWS.CloudWatch();
-
-  // Interate through each of the endpoints provided.
-  var endpoints = event;
+  const endpoints = event;
   console.log('endpoints:', endpoints);
 
-  endpoints.forEach(
-
-    function(endpoint) {
-      console.log("Requesting " + endpoint);
-
-      var requestObj = {
-        "uri": endpoint,
-        "time": true,
-        "timeout": 10000, // milliseconds
+  const requests = endpoints.map(async (endpoint) => {
+    try {
+      const response = await axios.get(endpoint, { timeout: 10000 });
+      output[endpoint] = {
+        statusCode: response.status,
+        durationMS: response.headers['response-time']
       };
+    } catch (error) {
+      output[endpoint] = {
+        HTTPError: error.code || 'Network Error',
+        statusCode: 0,
+        durationMS: 0
+      };
+    }
+    console.log(endpoint + " : " + JSON.stringify(output[endpoint]));
 
-      request(requestObj, function (error, response, body) {
-        // Debug only - goes to CloudWatch logs.
-        //console.log('error:', error);
-        //console.log('statusCode:', response && response.statusCode);
-
-        // Create the stats for this request.
-        if (error) {
-          output[ endpoint ] = {
-            "HTTPError": error.code,
-            "statusCode": 0,
-            "durationMS": 0,
-          };
-        } else {
-          output[ endpoint ] = {
-            "statusCode": response.statusCode,
-            "durationMS": response.timingPhases.total
-          };
-        }
-
-        console.log(endpoint +" : "+ JSON.stringify(output [ endpoint ]));
-
-
-
-        // Push metrics to CloudWatch.
-        var params = {
-          Namespace: 'Lambda-Ping/HTTP',
-          MetricData: [
-            // StatusCode
+    // Push metrics to CloudWatch.
+    const params = {
+      Namespace: 'Lambda-Ping/HTTP',
+      MetricData: [
+        // StatusCode
+        {
+          MetricName: 'StatusCode',
+          Dimensions: [
             {
-              MetricName: 'StatusCode',
-              Dimensions: [
-                {
-                  Name: 'Endpoint',
-                  Value: endpoint
-                }
-              ],
-              StatisticValues: {
-                SampleCount: 1,
-                Sum: output[ endpoint ]["statusCode"],
-                Minimum: 0,
-                Maximum: 1000, // HTTP spec permits any three-digit status code
-              },
-              Unit: 'None'
-            },
-            // Latency (Response Time)
-            {
-              MetricName: 'Latency',
-              Dimensions: [
-                {
-                  Name: 'Endpoint',
-                  Value: endpoint
-                }
-              ],
-              StatisticValues: {
-                SampleCount: 1,
-                Sum: output[ endpoint ]["durationMS"],
-                Minimum: 0,
-                Maximum: 30000, // 30 seconds
-              },
-              Unit: 'Milliseconds'
+              Name: 'Endpoint',
+              Value: endpoint
             }
-          ]
+          ],
+          StatisticValues: {
+            SampleCount: 1,
+            Sum: output[endpoint].statusCode,
+            Minimum: 0,
+            Maximum: 1000, // HTTP spec permits any three-digit status code
+          },
+          Unit: 'None'
+        },
+        // Latency (Response Time)
+        {
+          MetricName: 'Latency',
+          Dimensions: [
+            {
+              Name: 'Endpoint',
+              Value: endpoint
+            }
+          ],
+          StatisticValues: {
+            SampleCount: 1,
+            Sum: output[endpoint].durationMS,
+            Minimum: 0,
+            Maximum: 30000, // 30 seconds
+          },
+          Unit: 'Milliseconds'
         }
+      ]
+    };
 
-        cloudwatch.putMetricData(params, function(error, data) {
-          if (error) {
-            console.log("Unexpected issue posting metrics to CloudWatch");
-            console.log(error, error.stack);
-          } else {
-            console.log("Logged metrics in Cloudwatch at: "+ params['Namespace']);
-          }
-        });
+    await cloudwatch.putMetricData(params).promise();
+    console.log("Logged metrics in CloudWatch at: " + params['Namespace']);
+  });
 
-      });
-    }
-  );
+  await Promise.all(requests);
 
-
-  // We need to wait for all the callbacks to complete, otherwise we'll end up
-  // not returning any, or only a subset, of the results.
-  waitForCompletion();
-
-  function waitForCompletion() {
-    // Count of output objects should match count of endpoints
-    if (Object.keys(output).length < endpoints.length) {
-      setTimeout(waitForCompletion, 100);
-      return;
-    }
-
-    // Log the finalised output object, as well as returning it to the requester.
-    console.log("Final results:");
-    console.log(JSON.stringify(output));
-    callback(null, output);
-  }
-
+  // Log the finalised output object, as well as returning it to the requester.
+  console.log("Final results:");
+  console.log(JSON.stringify(output));
+  return output;
 };
